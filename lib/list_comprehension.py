4a435fb7299b8696f567c40524ee0ccdca953eab
abdiqafar <iabdiqafar24@gmail.com>
2024-11-27 17:22:19 +0300

    commit

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # Using a list comprehension, write a function return_evens() that returns a list of all of the even elements of a sequence of integers.
+    return [num for num in num_list if num % 2 == 0]
+numbers = [0, 1, 3, 5, 7, 8, 9]
+print(return_evens(numbers))
+
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+sentences = ["Hello", "I'm doing great", "Python is fun"]
+print(make_exclamation(sentences))
